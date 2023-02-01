403b8d4291dac05d4bc45db8edbe1fbf2ea4f6e2
ngoctuds73 <123917622+ngoctuds73@users.noreply.github.com>
2023-02-01 09:40:32 +0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,21 +2,25 @@ class Calculator:
     def __init__(self, *args):
         self.args = args
 
+    @staticmethod
     def add(*args):
         return sum(args)
 
+    @staticmethod
     def multiply(*args):
         result = 1
         for i in args:
             result *= i
         return result
 
+    @staticmethod
     def divide(*args):
         result = args[0]
         for i in args[1:]:
             result /= i
         return result
 
+    @staticmethod
     def subtract(*args):
         result = args[0]
         for i in args[1:]:
